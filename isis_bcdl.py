b9323b1fc23822334a4fe64bc79a9383b281097d
Gonzalo Díaz <gdiazmez@cisco.com>
2020-04-09 12:30:15 -0400

    ISIS Priority Testcase

---
+++ isis_bcdl.py
@@ -0,0 +1,128 @@
+"""Implementation ISIS triggers"""
+
+import logging
+import secrets
+import re
+from pyats import aetest
+from genie.harness.base import Trigger
+from utils import apply_template
+from parsers import route_detail_parser
+import pprint
+
+
+log = logging.getLogger(__name__)
+
+class TriggerIsisBcdl(Trigger):
+
+    @aetest.setup
+    def cfg_isis_setup(self, testbed, steps, message, Test_Prefix1, Test_Prefix2,
+                        ACL_Name_High, ACL_Name_Medium, templates_dir, isis_cfg):
+        log.info("Test case steps:\n{msg}".format(msg=message))
+
+        try:
+            xr1 = testbed.devices['uut']
+        except KeyError:
+            step.failed('Could not find XR device node "uut" in the testbed')
+            self.failed(goto=['next_tc'])
+
+        with steps.start("Discovering ISIS process ID") as step:
+
+            isis_parsed = xr1.parse('show isis')
+            process_id = ''
+            for instances in isis_parsed['instance'].keys():
+                process_id = isis_parsed['instance'][instances]['process_id']
+                vrf = isis_parsed['instance'][instances]['vrf']
+                if (vrf == 'default'):
+                    break
+
+            log.info("ISIS process name for VRF default is {process}".format(process=process_id))
+
+        with steps.start("Configure ISIS SPF Priority") as step:
+            self.commit_label = 'pyats_{secret}'.format(secret=secrets.token_urlsafe(10))
+
+            success, conf_message = apply_template(xr1, templates_dir, isis_cfg,
+                                                process_id = process_id,
+                                                ACL_Name_High = ACL_Name_High,
+                                                ACL_Name_Medium = ACL_Name_Medium,
+                                                Prefix_High = Test_Prefix1,
+                                                Prefix_Medium = Test_Prefix2,
+                                                commit_label=self.commit_label)
+
+            if not success:
+                log.info("Configuration failed with: {msg}".format(msg=conf_message))
+                self.failed(goto=['next_tc'])
+
+    @aetest.test
+    def test_routes(self, steps, uut, Test_Prefix1, Test_Prefix2, Test_Prefix3):
+
+        with steps.start("Testing High Priority Route") as step:
+            out1 = uut.execute('show route {route} detail'.format(route=Test_Prefix1))
+            out1_parsed = route_detail_parser(out1)
+
+            route_priority = out1_parsed['prefix'][Test_Prefix1]['route_priority']
+            if re.search(r'HIGH', route_priority):
+                log.info("Prefix {prefix} has route priority {priority}".format(prefix=Test_Prefix1,
+                                                                                priority=route_priority))
+            else:
+                step.failed("Prefix {prefix} has incorrect route priority {priority}".format(prefix=Test_Prefix1,
+                                                                                priority=route_priority))
+
+        with steps.start("Testing Medium Priority Route") as step:
+            out2 = uut.execute('show route {route} detail'.format(route=Test_Prefix2))
+            out2_parsed = route_detail_parser(out2)
+
+            route_priority = out2_parsed['prefix'][Test_Prefix2]['route_priority']
+            if re.search(r'MEDIUM', route_priority):
+                log.info("Prefix {prefix} has route priority {priority}".format(prefix=Test_Prefix2,
+                                                                                priority=route_priority))
+            else:
+                step.failed("Prefix {prefix} has incorrect route priority {priority}".format(prefix=Test_Prefix2,
+                                                                                priority=route_priority))
+
+        with steps.start("Testing Low Priority Route") as step:
+            out3 = uut.execute('show route {route} detail'.format(route=Test_Prefix3))
+            out3_parsed = route_detail_parser(out3)
+
+            route_priority = out3_parsed['prefix'][Test_Prefix3]['route_priority']
+            if re.search(r'LOW', route_priority):
+                log.info("Prefix {prefix} has route priority {priority}".format(prefix=Test_Prefix3,
+                                                                                priority=route_priority))
+            else:
+                step.failed("Prefix {prefix} has incorrect route priority {priority}".format(prefix=Test_Prefix3,
+                                                                                priority=route_priority))
+
+    @aetest.cleanup
+    def uncfg_isis_setup(self, testbed, steps, message,
+                        ACL_Name_High, ACL_Name_Medium, templates_dir, isis_uncfg):
+        log.info("Test case steps:\n{msg}".format(msg=message))
+
+        try:
+            xr1 = testbed.devices['uut']
+        except KeyError:
+            step.failed('Could not find XR device node "uut" in the testbed')
+            self.failed(goto=['next_tc'])
+
+        with steps.start("Discovering ISIS process ID") as step:
+
+            isis_parsed = xr1.parse('show isis')
+            process_id = ''
+            for instances in isis_parsed['instance'].keys():
+                process_id = isis_parsed['instance'][instances]['process_id']
+                vrf = isis_parsed['instance'][instances]['vrf']
+                if (vrf == 'default'):
+                    break
+
+            log.info("ISIS process name for VRF default is {process}".format(process=process_id))
+
+        with steps.start("Cleanup for ISIS SPF Priority") as step:
+            self.commit_label = 'pyats_{secret}'.format(secret=secrets.token_urlsafe(10))
+
+            success, conf_message = apply_template(xr1, templates_dir, isis_uncfg,
+                                                process_id = process_id,
+                                                ACL_Name_High = ACL_Name_High,
+                                                ACL_Name_Medium = ACL_Name_Medium,
+                                                commit_label=self.commit_label)
+
+            if not success:
+                log.info("Configuration failed with: {msg}".format(msg=conf_message))
+                self.failed(goto=['next_tc'])
---
+++ parsers.py
@@ -0,0 +1,133 @@
+import re
+
+def inventory_parser(output):
+    # Init vars
+    inventory_dict = {}
+
+    # NAME: "module 0/RSP0/CPU0", DESCR: "ASR9K Route Switch Processor with 440G/slot Fabric and 6GB"
+    # NAME: "Rack 0", DESCR: "Cisco XRv9K Centralized Virtual Router"
+    # NAME: "Rack 0", DESCR: "Sherman 1RU Chassis with 24x400GE QSFP56-DD & 12x100G QSFP28"
+    # NAME: "0/FT4", DESCR: "Sherman Fan Module Reverse Airflow / exhaust, BLUE"
+    # NAME: "TenGigE0/0/0/0", DESCR: "Cisco SFP+ 10G SR Pluggable Optics Module"
+    p1 = re.compile(r'^NAME: +\"(?P<module_name>[\S\s]*)\",'
+                    r' +DESCR: +\"(?P<descr>[\S\s]*)\"$')
+
+    # PID: A9K-MPA-20X1GE, VID: V02, SN: FOC1811N49J
+    # PID: SFP-1G-NIC-X      , VID: N/A, SN: N/A
+    # PID: N/A, VID: N/A, SN:
+    p2 = re.compile(r'^PID: *(?P<pid>[\S\s]*),'
+                    r' +VID: *(?P<vid>[\S\s]*),'
+                    r' SN: *(?P<sn>[\S\s]*)$')
+
+    for line in output.splitlines():
+        line = line.strip()
+        if not line:
+            continue
+
+        # NAME: "0/FT4", DESCR: "Sherman Fan Module Reverse Airflow / exhaust, BLUE"
+        # NAME: "TenGigE0/0/0/0", DESCR: "Cisco SFP+ 10G SR Pluggable Optics Module"
+        m = p1.match(line)
+        if m:
+            if 'module_name' not in inventory_dict:
+                inventory_dict['module_name'] = {}
+            module_name = str(m.groupdict()['module_name'])
+            if module_name not in inventory_dict['module_name']:
+                inventory_dict['module_name'][module_name] = {}
+                inventory_dict['module_name'][module_name]['descr'] = \
+                    str(m.group('descr')).strip()
+                continue
+
+        # PID: A9K-MPA-20X1GE, VID: V02, SN: FOC1811N49J
+        # PID: SFP-1G-NIC-X      , VID: N/A, SN: N/A
+        m = p2.match(line)
+        if m:
+            inventory_dict['module_name'][module_name]['pid'] = \
+                str(m.group('pid')).strip()
+            inventory_dict['module_name'][module_name]['vid'] = \
+                str(m.group('vid')).strip()
+            inventory_dict['module_name'][module_name]['sn'] = \
+                str(m.group('sn')).strip()
+            continue
+    return inventory_dict
+
+def controller_parser(output):
+
+    # Init vars
+    controller_dict = {}
+
+    #   T1 0/10/0 is up.
+    p1 = re.compile(r'^(?P<if_name>[\S\s]* [\S\s]*) is (?P<state>[\S\s]*)\.$')
+
+    #   Applique type is NCS4200-48T1E1-CE
+    p2 = re.compile(r'^Applique type is +(?P<module_name>[\S\s]*)')
+
+    #   DSX1 BERT pattern     : 2^23
+    p3 = re.compile(r'^DSX1 BERT pattern +: +(?P<bert_pattern>[\S\s]*)')
+
+    for line in output.splitlines():
+        line = line.strip()
+        if not line:
+            continue
+
+        #   T1 0/10/0 is up.
+        m = p1.match(line)
+        if m:
+            if 'if_name' not in controller_dict:
+                controller_dict['if_name'] = {}
+            if_name = str(m.groupdict()['if_name'])
+            if if_name not in controller_dict['if_name']:
+                controller_dict['if_name'][if_name] = {}
+                controller_dict['if_name'][if_name]['status'] = \
+                    str(m.group('state')).strip()
+                continue
+
+        #   Applique type is NCS4200-48T1E1-CE
+        m = p2.match(line)
+        if m:
+            controller_dict['if_name'][if_name]['module_name'] = \
+                str(m.group('module_name')).strip()
+            continue
+
+        #   DSX1 BERT pattern     : 2^23
+        m = p3.match(line)
+        if m:
+            controller_dict['if_name'][if_name]['bert_pattern'] = \
+                str(m.group('bert_pattern')).strip()
+            continue
+
+    return controller_dict
+
+def route_detail_parser(output):
+
+    # Init vars
+    out_dict = {}
+
+    # Routing entry for 172.16.12.2/32
+    p1 = re.compile(r'^Routing entry for (?P<prefix>[\S\s]*)$')
+
+    # Route Priority: RIB_PRIORITY_NON_RECURSIVE_LOW (8) SVD Type RIB_SVD_TYPE_LOCAL
+    p2 = re.compile(r'^Route Priority: (?P<route_priority>[\S\s]*) \(.*$')
+
+    for line in output.splitlines():
+        line = line.strip()
+        if not line:
+            continue
+
+        # Routing entry for 172.16.12.2/32
+        m = p1.match(line)
+        if m:
+            if 'prefix' not in out_dict:
+                out_dict['prefix'] = {}
+            prefix = str(m.groupdict()['prefix'])
+            if prefix not in out_dict['prefix']:
+                out_dict['prefix'][prefix] = {}
+                continue
+
+        # Route Priority: RIB_PRIORITY_NON_RECURSIVE_LOW (8) SVD Type RIB_SVD_TYPE_LOCAL
+        m = p2.match(line)
+        if m:
+            out_dict['prefix'][prefix]['route_priority'] = \
+                str(m.group('route_priority')).strip()
+            continue
+
+    return out_dict
