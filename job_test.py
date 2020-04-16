a97aa32cc4a5bac16e7dbeb704e10c0851efea40
Gonzalo Díaz <gdiazmez@cisco.com>
2020-04-16 12:25:22 -0400

    Segment Routing TC

--- job_test.py
+++ job_test.py
@@ -10,5 +10,5 @@ def main():
 
     gRun(mapping_datafile='mapping_datafile.yaml',
          trigger_datafile='trigger_datafile.yaml',
-         trigger_groups=And('trigger','isis_bcdl'),
+         trigger_groups=And('trigger','sr_oper'),
          subsection_datafile='subsection_datafile.yaml')
