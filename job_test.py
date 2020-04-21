570dbf70740a259e6708329a6489f4933547d95f
Gonzalo Díaz <gdiazmez@cisco.com>
2020-04-21 14:43:32 -0400

    xr environment change

--- job_test.py
+++ job_test.py
@@ -10,5 +10,5 @@ def main():
 
     gRun(mapping_datafile='mapping_datafile.yaml',
          trigger_datafile='trigger_datafile.yaml',
-         trigger_groups=And('trigger','sr_oper'),
+         trigger_groups=And('trigger','isis_bcdl'),
          subsection_datafile='subsection_datafile.yaml')
